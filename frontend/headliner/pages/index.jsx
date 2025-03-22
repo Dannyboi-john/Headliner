88ce920c581441a8227b665f2b78717b3aeae7b7
danny <ddjjpp340@gmail.com>
2025-03-22 18:01:21 -0230

    Removed default next.js page

---
+++ frontend/headliner/pages/index.jsx
@@ -0,0 +1,7 @@
+import App from '../src/app/App.jsx';
+
+const IndexPage = () => {
+    return <App />;
+};
+
+export default IndexPage;
---
+++ frontend/headliner/src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+    return (
+        <>
+
+        <h3>Welcome 2 my web app :3</h3>
+
+        </>
+    )
+}
+
+export default App;
