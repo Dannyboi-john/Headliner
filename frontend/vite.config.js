373cc1a7d586dd8afa45b31333d4e0c503654211
danny <ddjjpp340@gmail.com>
2025-04-02 21:13:41 -0230

    Reconfigured .yml and vite.config to reflect saved changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: '0.0.0.0'
+    watch: {
+      usePolling: true,  // To be sure changes are detected in Docker.
+    },
+    host: true,  // Allows access from different network interfaces.
+    strictPort: true  // Prevents port conflicts.
   },
 })
