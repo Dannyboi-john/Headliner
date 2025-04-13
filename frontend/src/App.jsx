3aae6979c88e832370e60cb7a5fb985c8026b019
Mitchell Hynes <mitch@spellbook.legal>
2025-04-13 19:33:09 -0230

    Initial commit for TailwindCSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Register from './components/Register'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
