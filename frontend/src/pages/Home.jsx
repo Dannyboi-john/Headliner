e5d149e64d20bd24ef3ec8e1a4d30029fcd9b411
danny <ddjjpp340@gmail.com>
2025-04-21 18:17:30 -0230

    Added icon and title to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React /*, { useState } */ from 'react';
 import { Link } from 'react-router-dom';
 import icon from '../assets/icon-small-nobg.png';
+import bigIcon from '../assets/icon-big-nobg.png';
 
 const Home = () => {
     console.log("this is my home page  :)");
@@ -23,9 +24,20 @@ const Home = () => {
                     <span className="text-sm font-medium font-sans hover:underline underline-offset-4 cursor-pointer">
                         Contact
                     </span>
-
                 </nav>
             </header>
+
+            <main className="bg-gradient-to-t from-[#363636] to-[#242424] h-screen">
+                <div className="grid grid-cols-[2fr_1fr] items-center mt-[20%] ml-[5%]"> {/* Container for the body */}
+                    <div className="flex items-center"> {/* Container for the left 2/3rds: logo / header / description */}
+                        <img src={bigIcon} className="w-1/5"/>
+                        <p className="text-6xl font-primary pl-[5%]">Connect with Local Musicians</p>
+                    </div>
+                    <div> {/* Container for the right 1/3rd: login & register toggle buttons / login & register forms */}
+                    </div>
+                </div>
+            </main>
+
         </>
     )
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                primary: ['var(--font-primary)'],
+            },
+        },
+    },
+    plugins: [],
+}
