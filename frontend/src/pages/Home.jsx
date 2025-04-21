52b53c88334cf5094b0966dedf59b3268923c5ef
danny <ddjjpp340@gmail.com>
2025-04-21 12:05:00 -0230

    Resolved path error, removed background from logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React /*, { useState } */ from 'react';
 import { Link } from 'react-router-dom';
-import icon from '../assets.icon-small.png';
+import icon from '../assets/icon-small-nobg.png';
 
 const Home = () => {
     console.log("this is my home page  :)");
@@ -10,7 +10,7 @@ const Home = () => {
         <>
             <header className="px-4 lg:px-6 h-16 flex items-center border-b border-gray-300">
                 <Link href="/" className="flex items-center gap-2 font-semibold">
-                    <img src={icon} />
+                    <img src={icon} className="h-10"/>
                     <span>Headliner</span>
                 </Link>
                 <nav className="ml-auto flex gap-4 sm:gap-6">
