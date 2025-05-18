e21112c431ac9322823e34a4aaa349c91c45235d
danny <ddjjpp340@gmail.com>
2025-05-18 18:46:31 -0230

    Made the token appear in Dashboard on successful login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('./controllers/login');
 const registercontroller = require('./controllers/register');
+// const authFetchController = require('./authFetch');
 
 router.post('/login', loginController);
 router.post('/register', registercontroller);
+
+// router.post('/authFetch', authFetchController);
 
 module.exports = router;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import bigIcon from '../assets/icon-big-nobg.png';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+
+    const [token, setToken] = useState('');
+    // const navigate = useNavigate();
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('token');
+        setToken(accessToken || 'No token found');
+      }, []);
 
     console.log("You are now off the home page");
 
     return (
         <>
             <header className="px-4 lg:px-6 h-16 flex items-center border-b bg-blue-600 border-gray-300">
-                <Link href="/" className="flex items-center gap-2 font-semibold">
+                <Link href="/Dashboard" className="flex items-center gap-2 font-semibold">
                     <img src={bigIcon} className="h-10"/>
                     <span>Headliner</span>
                 </Link>
@@ -27,7 +37,11 @@ const Dashboard = () => {
             </header>
 
             <main className="bg-gradient-to-t from-purple-600 to-blue-600 h-screen">
-                <div>Look at you go!</div>
+                <div>
+                    <p className="text-wrap">
+                        {token}
+                    </p>
+                </div>
             </main>
         </>
     )
